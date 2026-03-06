34a47697da983280fb8ed227034b2f9b3e0167d6
Vasu <vasujohri016@gmail.com>
2026-03-07 04:15:46 +0530

    added userController

---
+++ interviewhub-backend/src/controllers/user.controller.js
@@ -0,0 +1,37 @@
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-errors.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { User } from "../models/users.models.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password, fullName, authProvider } = req.body;
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "User with this email or username already exists");
+  }
+  const currentAuthProvider = authProvider || "local";
+  if (currentAuthProvider === "local" && !password) {
+    throw new ApiError(400, "Password is required for registration");
+  }
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email: email.toLowerCase(),
+    password,
+    fullName,
+    authProvider: currentAuthProvider,
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -forgotPasswordToken -forgotPasswordExpiry -__v",
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User registered successfully"));
+});
+
+export { registerUser };
