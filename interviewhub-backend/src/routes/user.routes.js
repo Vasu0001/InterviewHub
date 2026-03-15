aeea2adef63a11357cc3c8d26715fe8ebd933945
Vasu <vasujohri016@gmail.com>
2026-03-15 18:54:00 +0530

    connect login route

--- interviewhub-backend/src/routes/user.routes.js
+++ interviewhub-backend/src/routes/user.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { userRegisterValidator } from "../validators/index.js";
+import {
+  userRegisterValidator,
+  userLogInValidator,
+} from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
-
+router.route("/login").post(userLogInValidator(), validate, loginUser);
 export default router;
