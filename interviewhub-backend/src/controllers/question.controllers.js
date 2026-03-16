a0fe7a4f260369cdfa195eee92af6318c7481998
Vasu <vasujohri016@gmail.com>
2026-03-17 03:30:38 +0530

    add createQuestion function

---
+++ interviewhub-backend/src/controllers/question.controllers.js
@@ -0,0 +1,24 @@
+import { ApiError } from "../utils/api-errors.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { Question } from "../models/question.model.js";
+
+const createQuestion = asyncHandler(async (req, res) => {
+  const { title, description, difficulty, category, startingCode } = req.body;
+  const question = await Question.findOne({ title });
+  if (question) {
+    throw new ApiError(400, "A question with this title already exists");
+  }
+  const newQuestion = await Question.create({
+    title,
+    description,
+    difficulty,
+    category,
+    startingCode,
+  });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, newQuestion, "Question created"));
+});
+
+export { createQuestion };
