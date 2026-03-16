c6f401a23b57046634e9314012c92b5553e18e25
Vasu <vasujohri016@gmail.com>
2026-03-16 23:00:47 +0530

    write logout and current-user route

--- interviewhub-backend/src/routes/user.routes.js
+++ interviewhub-backend/src/routes/user.routes.js
@@ -4,10 +4,18 @@ import {
   userLogInValidator,
 } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
-
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getCurrentUser,
+} from "../controllers/user.controller.js";
+import { jwtVerify } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLogInValidator(), validate, loginUser);
+
+router.route("/current-user").get(jwtVerify, getCurrentUser);
+router.route("/logout").post(jwtVerify, logoutUser);
 export default router;
