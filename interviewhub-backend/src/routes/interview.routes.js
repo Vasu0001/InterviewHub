50fe7ff08793d0eeefa987624363263e8b5d51f1
Vasu <vasujohri016@gmail.com>
2026-04-21 02:29:59 +0530

    update interview.routes.js and write /history route

--- interviewhub-backend/src/routes/interview.routes.js
+++ interviewhub-backend/src/routes/interview.routes.js
@@ -4,12 +4,16 @@ import {
   createInterview,
   getInterviewByRoomId,
   submitAnswer,
+  submitFeedback,
+  getInterviewsHistory,
 } from "../controllers/interview.contollers.js";
 
 const router = Router();
 
 router.route("/").post(jwtVerify, createInterview);
+router.route("/history").get(jwtVerify, getInterviewsHistory);
 router.route("/:roomId").get(getInterviewByRoomId);
 router.route("/:roomId/submit").post(submitAnswer);
+router.route("/:roomId/feedback").patch(jwtVerify, submitFeedback);
 
 export default router;
